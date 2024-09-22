8fe45d58674ad999a17e540a05bc30682674e087
timberwlves <graysonf322@gmail.com>
2024-09-22 14:53:21 -0400

    Add files via upload

---
+++ module 5/Module 5 Exercise 1 Plan - Grayson Belmonte.py
@@ -0,0 +1,13 @@
+Planning for a Jeopardy program flowchart
+
+User is shown rules, display categories and point values
+
+Player chooses category 
+
+Display question, start timer, if answer is wrong pass to next player
+
+if answer is correct award points
+
+if questions remain loop back to choose category
+
+If no questions remain display winner and end game
