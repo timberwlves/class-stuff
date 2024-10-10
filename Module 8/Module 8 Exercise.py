c4c0927f80ffede96a5e310208cff122cb6ab492
timberwlves <graysonf322@gmail.com>
2024-10-10 19:29:58 -0400

    Add files via upload

---
+++ Module 8/Module 8 Exercise.py
@@ -0,0 +1,14 @@
+a = 2
+b = 3
+
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+result = greaterThan(a, b)
+
+print("The statement " + str(a) + " is greater than " + str(b) + " is " + str(result))
+
+    
